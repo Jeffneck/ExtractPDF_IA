821ab4349d4fdd8d9b832ed87095b50d5a8d583f
Hugo <hugot.anglade@gmail.com>
2025-01-06 11:08:22 -0500

    take imgs + modify naming imgs, maybe not ok

--- scripts/utils/image_utils.py
+++ scripts/utils/image_utils.py
@@ -2,13 +2,17 @@
 
 from pdf2image import convert_from_path
 import os
+from datetime import datetime
 
 def pdf_to_images(pdf_path, images_folder):
+    # Obtenir la date actuelle au format AAAA_MM_JJ
+    date = datetime.now().strftime("%Y_%m_%d")
+
     if not os.path.exists(images_folder):
         os.makedirs(images_folder)
 
     pages = convert_from_path(pdf_path, dpi=200)
     for i, page in enumerate(pages):
-        output_path = os.path.join(images_folder, f"{os.path.splitext(os.path.basename(pdf_path))[0]}_page_{i+1}.png")
+        output_path = os.path.join(images_folder, f"{os.path.splitext(os.path.basename(pdf_path))[0]}_page_{i+1}_{date}.png")
         page.save(output_path, 'PNG')
         print(f"Enregistré : {output_path}")
